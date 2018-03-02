6b958af6ad40901a285f554c2d1f969422f58cfe
G1mi <g1mi@qq.com>
2018-03-03 00:58:42 +0800

    feat: 完成afterspeak页面功能，下一个展示页面功能

--- dist/util/Mock.js
+++ dist/util/Mock.js
@@ -18,15 +18,10 @@ var MockingData = function () {
     value: function getBulletinInfo() {
       return "\u4EB2\u7231\u7684\u987E\u5BA2\uFF0C\u4F60\u597D\uFF01\n    \u60A8\u662F\u4ECA\u5929\u7B2C101\u4E2A\u6562\u8BF4\u53D1\u8D77\u8005\u3002\n    \u8BF7\u6CE8\u610F\u53D1\u8D77\u7684\u6562\u8BF4\u4E0D\u5F97\u5305\u542B\u5982\u4E0B\u5185\u5BB9\uFF1A\n        \uD83C\uDE32\u8272\u60C5\uFF0C\n        \uD83C\uDE32\u8FDD\u6CD5\uFF0C\n        \uD83C\uDE32\u653F\u6CBB\u76F8\u5173\u3002\n     \u60F3\u770B\u770B\u6628\u5929\u7684\u6570\u636E\u4E48\uFF1F";
     }
-  }, {
-    key: "getAllowedCities",
-    value: function getAllowedCities() {
-      return ['宝鸡市', "汉中市"];
-    }
   }]);
 
   return MockingData;
 }();
 
 exports.default = MockingData;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIk1vY2suanMiXSwibmFtZXMiOlsiTW9ja2luZ0RhdGEiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7SUFBcUJBLFc7Ozs7Ozs7c0NBQ007QUFDdkI7QUFPRDs7O3VDQUMwQjtBQUN6QixhQUFPLENBQUMsS0FBRCxFQUFRLEtBQVIsQ0FBUDtBQUNEOzs7Ozs7a0JBWmtCQSxXIiwiZmlsZSI6Ik1vY2suanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBjbGFzcyBNb2NraW5nRGF0YSB7XG4gIHN0YXRpYyBnZXRCdWxsZXRpbkluZm8oKSB7XG4gICAgcmV0dXJuIGDkurLniLHnmoTpob7lrqLvvIzkvaDlpb3vvIFcbiAgICDmgqjmmK/ku4rlpKnnrKwxMDHkuKrmlaLor7Tlj5HotbfogIXjgIJcbiAgICDor7fms6jmhI/lj5HotbfnmoTmlaLor7TkuI3lvpfljIXlkKvlpoLkuIvlhoXlrrnvvJpcbiAgICAgICAg8J+IsuiJsuaDhe+8jFxuICAgICAgICDwn4iy6L+d5rOV77yMXG4gICAgICAgIPCfiLLmlL/msrvnm7jlhbPjgIJcbiAgICAg5oOz55yL55yL5pio5aSp55qE5pWw5o2u5LmI77yfYFxuICB9XG4gIHN0YXRpYyBnZXRBbGxvd2VkQ2l0aWVzICgpIHtcbiAgICByZXR1cm4gWyflrp3puKHluIInLCBcIuaxieS4reW4glwiXVxuICB9XG5cbn1cbiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIk1vY2suanMiXSwibmFtZXMiOlsiTW9ja2luZ0RhdGEiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7SUFBcUJBLFc7Ozs7Ozs7c0NBQ007QUFDdkI7QUFPRDs7Ozs7O2tCQVRrQkEsVyIsImZpbGUiOiJNb2NrLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgY2xhc3MgTW9ja2luZ0RhdGEge1xuICBzdGF0aWMgZ2V0QnVsbGV0aW5JbmZvKCkge1xuICAgIHJldHVybiBg5Lqy54ix55qE6aG+5a6i77yM5L2g5aW977yBXG4gICAg5oKo5piv5LuK5aSp56ysMTAx5Liq5pWi6K+05Y+R6LW36ICF44CCXG4gICAg6K+35rOo5oSP5Y+R6LW355qE5pWi6K+05LiN5b6X5YyF5ZCr5aaC5LiL5YaF5a6577yaXG4gICAgICAgIPCfiLLoibLmg4XvvIxcbiAgICAgICAg8J+Isui/neazle+8jFxuICAgICAgICDwn4iy5pS/5rK755u45YWz44CCXG4gICAgIOaDs+eci+eci+aYqOWkqeeahOaVsOaNruS5iO+8n2BcbiAgfVxufVxuIl19
---
+++ src/util/config.js
@@ -0,0 +1,13 @@
+export default config => {
+  let con = {
+    howLong: '1',
+    allowedCities: ['宝鸡市', '汉中市'],
+    howMany: '50',
+    unlimitNo: false,
+
+    //----------afterspeak---------------
+    beforPosChosed: '点击选取地图坐标',
+    afterPosChosed: '点击重新选取坐标'
+  }
+  return con
+}
