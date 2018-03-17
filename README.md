ca1cb9dac2c017cd508476d64ea61dd87a964578
G1mi <g1mi@qq.com>
2018-03-17 17:30:50 +0800

    文件清理

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # 吉米敢说
-吉米敢说
+学习wepy 的制作的软件，后期做完应该会上线
+
+## 使用
+```
+git clone https://github.com/g1mi/JMGS.git your-path
+cd your-path
+npm i  //等待安装完所需要的包
+npm run dev
+上面步骤完成后用微信开发者工具导入目录下dist文件夹即可
+```
+### 用到的包
+> moment.js
+> xstream.js
+
+
+### license
+MIT
--- src/util/config.js
+++ src/util/config.js
@@ -1,18 +1,23 @@
 export default config => {
   let con = {
+    //----------map---------------宝鸡
+    defaultLongitude: "107.235982",
+    defaultLatitude: "34.360620",
+    //----------afterspeak---------------
+    beforPosChosed: '点击选取地图坐标',
+    afterPosChosed: '点击重新选取坐标',
     howLong: '1',
     allowedCities: ['宝鸡市', '汉中市'],
     howMany: '50',
     unlimitNo: false,
 
-    //----------afterspeak---------------
-    beforPosChosed: '点击选取地图坐标',
-    afterPosChosed: '点击重新选取坐标',
-
-
-    //user limitations
-    maxGanshuoUp: 1,
-    maxGanshuoIn: 5
+    //----------beforespeak---------------
+    readyToRecord: "准备说：",
+    buttonTip: "请长按录音...",
+    recording: "正在录音...",
+    canceled: "已取消!",
+    waitAWhile: "请稍后...",
+    cancelRecording: "向上滑动松开 取消录音"
   }
   return con
 }
