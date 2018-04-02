89f7db64bd038320e9756d7ee2f671c39e41c3c7
G1mi <g1mi@qq.com>
2018-04-02 13:42:19 +0800

    v0.1.0: 基础功能完善

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # 吉米敢说
+
 学习wepy 的制作的软件，后期做完应该会上线
 
 ## 使用
+
 ```
 git clone https://github.com/g1mi/JMGS.git your-path
 cd your-path
@@ -9,10 +11,32 @@ npm i  //等待安装完所需要的包
 npm run dev
 上面步骤完成后用微信开发者工具导入目录下dist文件夹即可
 ```
-### 用到的包
-> moment.js
-> xstream.js
 
+### 当前功能
+
+#### 基本功能
+
+- [x] 地图显示敢说- [x] 添加敢说
+- [x] 浏览敢说
+- [x] 添加敢说
+- [x] 浏览敢说记录
+
+#### 排序功能
+
+- [ ] 时间倒序
+- [ ] 热度
+
+#### 评分功能
+
+- [ ] 陌生人thumbsup thumbsdown
+- [ ] 挑战者thumbsup thumbsdown
+- [ ] 换算成绩，挑战后的若干时间内显示成绩
+
+### 用到的包
+
+> [moment.js](https://github.com/moment/moment)
+> [xstream.js](https://github.com/staltz/xstream)
 
 ### license
+
 MIT
--- src/util/qiniuUploader.js
+++ src/util/qiniuUploader.js
@@ -1,171 +1,41 @@
-// created by gpake
-(function () {
-
-  var config = {
-    qiniuRegion: '',
-    qiniuImageURLPrefix: '',
-    qiniuUploadToken: '',
-    qiniuUploadTokenURL: '',
-    qiniuUploadTokenFunction: null,
-    qiniuShouldUseQiniuFileName: false
-  }
-
-  module.exports = {
-    init: init,
-    upload: upload,
-  }
-
-  // 在整个程序生命周期中，只需要 init 一次即可
-  // 如果需要变更参数，再调用 init 即可
-  function init(options) {
-    config = {
-      qiniuRegion: '',
-      qiniuImageURLPrefix: '',
-      qiniuUploadToken: '',
-      qiniuUploadTokenURL: '',
-      qiniuUploadTokenFunction: null,
-      qiniuShouldUseQiniuFileName: false
-    };
-    updateConfigWithOptions(options);
-  }
-
-  function updateConfigWithOptions(options) {
-    if (options.region) {
-      config.qiniuRegion = options.region;
-    } else {
-      console.error('qiniu uploader need your bucket region');
-    }
-    if (options.uptoken) {
-      config.qiniuUploadToken = options.uptoken;
-    } else if (options.uptokenURL) {
-      config.qiniuUploadTokenURL = options.uptokenURL;
-    } else if (options.uptokenFunc) {
-      config.qiniuUploadTokenFunction = options.uptokenFunc;
-    }
-    if (options.domain) {
-      config.qiniuImageURLPrefix = options.domain;
-    }
-    config.qiniuShouldUseQiniuFileName = options.shouldUseQiniuFileName
-  }
-
-  function upload(filePath, success, fail, options, progress,params) {
-    if (null == filePath) {
-      console.error('qiniu uploader need filePath to upload');
-      return;
-    }
-    if (options) {
-      updateConfigWithOptions(options);
-    }
-    if (config.qiniuUploadToken) {
-      doUpload(filePath, success, fail, options, progress, params);
-    } else if (config.qiniuUploadTokenURL) {
-      getQiniuToken(function () {
-        doUpload(filePath, success, fail, options, progress, params);
-      });
-    } else if (config.qiniuUploadTokenFunction) {
-      config.qiniuUploadToken = config.qiniuUploadTokenFunction();
-      if (null == config.qiniuUploadToken && config.qiniuUploadToken.length > 0) {
-        console.error('qiniu UploadTokenFunction result is null, please check the return value');
-        return
-      }
-      doUpload(filePath, success, fail, options, progress, params);
-    } else {
-      console.error('qiniu uploader need one of [uptoken, uptokenURL, uptokenFunc]');
-      return;
-    }
-  }
-
-  function doUpload(filePath, success_cb, fail, options, progress, params) {
-    if (null == config.qiniuUploadToken && config.qiniuUploadToken.length > 0) {
-      console.error('qiniu UploadToken is null, please check the init config or networking');
-      return;
-    }
-    var url = uploadURLFromRegionCode(config.qiniuRegion);
-    var fileName = filePath.split('//')[1];
-    if (options && options.key) {
-      fileName = options.key;
-    }
-    var formData = {
-      'token': config.qiniuUploadToken,
-    };
-    if (!config.qiniuShouldUseQiniuFileName) {
-      formData['key'] = fileName
-    }
-
-    // 添加自定义变量
-    for(var key in params) {
-      formData[key] = params[key];
-    }
-    console.log(formData);
-    var uploadTask = wx.uploadFile({
-      url: url,
-      filePath: filePath,
-      name: 'file',
-      formData: formData,
-      success: function (res) {
-        try {
-          if(res.data) {
-            success_cb(res.data)
-          }
-        } catch (error) {
-          fail(error)
+function upload(filePath, success_cb, fail_cb, options, progress, params) {
+  const url = options.region; // 华北
+  console.log(url);
+  console.log(options.region);
+  const formData = {
+    'token': options.token,
+    'key': options.key
+  };
+  console.log(formData);
+  // 添加自定义变量
+  for (const key in params) {
+    formData[key] = params[key];
+  };
+  const uploadTask = wx.uploadFile({
+    url: url,
+    filePath: filePath,
+    name: 'file',
+    formData: formData,
+    success: function (res) {
+      try {
+        if (res.data) {
+          success_cb(res.data)
         }
-      },
-      fail: function (error) {
-        console.error(error);
-        if (fail) {
-          fail(error);
-        }
+      } catch (error) {
+        fail_cb(error)
       }
-    })
-
-    uploadTask.onProgressUpdate((res) => {
-      progress && progress(res)
-    })
-  }
-
-  function getQiniuToken(callback) {
-    wx.request({
-      url: config.qiniuUploadTokenURL,
-      success: function (res) {
-        var token = res.data.uptoken;
-        if (token && token.length > 0) {
-          config.qiniuUploadToken = token;
-          if (callback) {
-            callback();
-          }
-        } else {
-          console.error('qiniuUploader cannot get your token, please check the uptokenURL or server')
-        }
-      },
-      fail: function (error) {
-        console.error('qiniu UploadToken is null, please check the init config or networking: ' + error);
-      }
-    })
-  }
-
-  function uploadURLFromRegionCode(code) {
-    var uploadURL = null;
-    switch (code) {
-      case 'ECN':
-        uploadURL = 'https://up.qbox.me';
-        break;
-      case 'NCN':
-        uploadURL = 'https://up-z1.qbox.me';
-        break;
-      case 'SCN':
-        uploadURL = 'https://up-z2.qbox.me';
-        break;
-      case 'NA':
-        uploadURL = 'https://up-na0.qbox.me';
-        break;
-      case 'ASG':
-        uploadURL = 'https://up-as0.qbox.me';
-        break;
-      default:
-        console.error('please make the region is with one of [ECN, SCN, NCN, NA, ASG]');
+    },
+    fail: function (error) {
+      console.error(error);
+      fail_cb(error);
     }
-    return uploadURL;
-  }
+  })
 
-})();
+  uploadTask.onProgressUpdate((res) => {
+    progress && progress(res)
+  })
+};
+
+module.exports = {
+  upload: upload
+};
